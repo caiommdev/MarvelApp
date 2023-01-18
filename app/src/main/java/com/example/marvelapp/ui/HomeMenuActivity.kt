eab2c320f6224d149f9aee7102fd36f017e3e7e2
caiommdev <caiomotamarinho22@gmail.com>
2023-01-18 20:47:40 -0300

    changed home menu layout to linearlayout

--- app/src/main/java/com/example/marvelapp/ui/HomeMenuActivity.kt
+++ app/src/main/java/com/example/marvelapp/ui/HomeMenuActivity.kt
@@ -1,11 +1,14 @@
-package com.example.marvelapp
+package com.example.marvelapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvelapp.R
 
 class HomeMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_menu)
     }
+
+    
 }
