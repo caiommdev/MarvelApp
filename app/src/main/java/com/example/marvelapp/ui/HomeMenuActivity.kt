0560ebc9a44d3bbdfde90b40be3d898b98321753
caiommdev <caiomotamarinho22@gmail.com>
2023-01-18 21:52:11 -0300

    added binding and setup

--- app/src/main/java/com/example/marvelapp/ui/HomeMenuActivity.kt
+++ app/src/main/java/com/example/marvelapp/ui/HomeMenuActivity.kt
@@ -3,12 +3,32 @@ package com.example.marvelapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.ActivityHomeMenuBinding
 
 class HomeMenuActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_menu)
+
+        binding = ActivityHomeMenuBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setup()
     }
 
-    
+    private fun setup() {
+        binding.apply {
+            homeMenuButtonCharacters.setOnClickListener {
+                //TODO:
+            }
+
+            homeMenuButtonSeries.setOnClickListener {
+                //TODO:
+            }
+        }
+    }
 }
